b5a2d73238cb9b9c9c8461dcbecff77b8e719704
OdairVasconcellos <124107318+OdairVasconcellos@users.noreply.github.com>
2023-02-23 21:27:32 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+function dois() {
+
+var umn = Number (document.getElementById("umn").value);
+var doisn = Number (document.getElementById("doisn").value);
+
+let resultado = [umn, doisn];
+
+document.getElementById("resultado").innerHTML = resultado;
+
+}
+
