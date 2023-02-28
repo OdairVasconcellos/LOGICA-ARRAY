d1a91b3aa9beafa3cc2613199774e3a009b64d54
OdairVasconcellos <124107318+OdairVasconcellos@users.noreply.github.com>
2023-02-27 23:39:42 -0300

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,41 @@
-function dois() {
+function tres() {
 
-var umn = Number (document.getElementById("umn").value);
-var doisn = Number (document.getElementById("doisn").value);
+    var a = Number(document.getElementById("a").value);
+    var b = Number(document.getElementById("b").value);
+    var c = Number(document.getElementById("c").value);
 
-let resultado = [umn, doisn];
+    let resultado = [a, b, c];
+    let resultado2 = [a, c, b];
+    let resultado3 = [b, a, c];
+    let resultado4 = [b, c, a];
+    let resultado5 = [c, a, b];
+    let resultado6 = [c, b, a];
 
-document.getElementById("resultado").innerHTML = resultado;
+
+    if (a > b && a > c) {
+        resultado = resultado
+    }
+    else if (a > c && c > b) {
+        resultado2 = resultado2
+    }
+    else if (b > a && a > c) {
+        resultado3 = resultado3
+    }
+    else if (b > c && c > a) {
+        resultado4 = resultado4
+    }
+    else if (c > a && a > b) {
+        resultado5 = resultado5
+    }
+    else if (c > b && b > a) {
+        resultado6 = resultado6
+    }
+    else {
+    }
+
+
+
+    document.getElementById("crescente").innerHTML = resultado;
 
 }
 
